600d47355eac37c50f84a05da3c67b45b00be9e1
andre-valman <andre.valmanaiav@gmail.com>
2022-06-29 14:18:38 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
+import Scroll from "./Scroll";
 import SearchBox from "./SearchBox";
 import "./App.css";
 
@@ -8,9 +8,16 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  //use useEffect to fetch data from robots
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -23,13 +30,19 @@ class App extends React.Component {
         .toLocaleLowerCase()
         .includes(this.state.searchfield.toLocaleLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
